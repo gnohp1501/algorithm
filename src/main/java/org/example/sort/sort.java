fa432c36d94fdfacf11c59399a49b893cebb8551
DESKTOP-LFJ0NUT\velle <vellev7759@gmail.com>
2023-01-29 21:41:55 +0700

    sort 1

---
+++ src/main/java/org/example/sort/sort.java
@@ -0,0 +1,63 @@
+package org.example.sort;
+
+/**
+ * @author NNPhong
+ * @since 1/29/2023
+ */
+public class sort {
+
+    public static void main(String[] args) {
+        sort ob = new sort();
+        int[] arr = {64, 25, 12, 22, 11};
+        //ob.selectionSort(arr);
+        //ob.bubbleSort(arr);
+        //ob.insertionSort(arr);
+        System.out.println("Sorted array");
+        ob.printArray(arr);
+    }
+
+    void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min_idx = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min_idx]) {
+                    min_idx = j;
+                    int temp = arr[min_idx];
+                    arr[min_idx] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+    }
+
+    void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+
+    void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    void printArray(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+}
