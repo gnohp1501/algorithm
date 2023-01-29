7138277cc76aa8bcaf79a8f2e09c2dc9231d8a24
DESKTOP-LFJ0NUT\velle <vellev7759@gmail.com>
2023-01-29 22:10:18 +0700

    sort 2

--- src/main/java/org/example/sort/sort.java
+++ src/main/java/org/example/sort/sort.java
@@ -9,7 +9,7 @@ public class sort {
     public static void main(String[] args) {
         sort ob = new sort();
         int[] arr = {64, 25, 12, 22, 11};
-        //ob.selectionSort(arr);
+        ob.selectionSort(arr);
         //ob.bubbleSort(arr);
         //ob.insertionSort(arr);
         System.out.println("Sorted array");
@@ -20,13 +20,12 @@ public class sort {
         for (int i = 0; i < arr.length - 1; i++) {
             int min_idx = i;
             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < arr[min_idx]) {
-                    min_idx = j;
-                    int temp = arr[min_idx];
-                    arr[min_idx] = arr[i];
-                    arr[i] = temp;
-                }
+                if (arr[j] < arr[min_idx]) min_idx = j;
             }
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+            printArray(arr);
         }
     }
 
